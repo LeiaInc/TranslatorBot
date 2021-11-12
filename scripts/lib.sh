0dfa1dc9cd0e831445346aae81a6bf567920d113
Clinton Pinto <ctpinto27@gmail.com>
2021-11-12 12:54:12 -0800

    Add SSH commit

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -19,6 +19,7 @@ _commit_if_needed() {
 _setup_git() {
   git config --global user.name "leiapixadmins"
   git config --global user.email "leiapixadmins@leiainc.com"
+  git remote set-url origin git@github.com:LeiaInc/TranslatorBot.git
 
   git checkout "${GITHUB_BASE_REF}"
   git pull
